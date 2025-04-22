93517c71941c3411c8f51e57f2998f4fc203215b
Agustín <managustin01@gmail.com>
2025-04-21 23:00:32 -0300

    managing props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <Button backgroundColor='#10b981'/>
+      <Button backgroundColor='#10b981' fontSize={30} pillShape={true}/>
     </>
   )
 }
--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,9 +1,23 @@
-export default function Button(props: {
-    backgroundColor: string;
+export default function Button({    //added some destructuring for the props, which is a common practice
+    backgroundColor,
+    fontSize,
+    pillShape,
+}:  {
+    backgroundColor: string
+    fontSize: number
+    pillShape: boolean
 }) {
 
+    console.log('Valor de pillShape: ', pillShape)
+    if (pillShape){
+        return (
+            <button style={{background: backgroundColor, fontSize: fontSize}} className="text-white rounded-[50px] px-4 py-2 border-2">
+                Cliqueame
+            </button>
+        )
+    }
     return (
-        <button style={{background: props.backgroundColor}} className="bg-blue-500 text-white rounded px-4 py-2">
+        <button style={{background: backgroundColor, fontSize: fontSize}} className="text-white rounded px-4 py-2">
             Cliqueame
         </button>
     )
