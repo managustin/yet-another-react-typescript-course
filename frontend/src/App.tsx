786a0f916d133eb270c255df56e03c4f5ced3c2b
Agustín <managustin01@gmail.com>
2025-04-22 12:16:32 -0300

    using children properly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,12 @@ import './App.css'
 import Button from './components/button'
 
 function App() {
-
   return (
-    <>
-      <Button style={{
-        backgroundColor: "blue",
-        fontSize: 25,
-        color: "white",
-        padding: "1rem 2rem",
-        borderWidth: 2,
-        borderColor: "black"
-      }}
-      borderRadius={{
-        topLeft: 10,
-        topRight: 20,
-        bottomLeft: 20,
-        bottomRight: 10
-      }}/>
-    </>
+    <main>
+
+      <Button>Cliqueame</Button>
+
+    </main>
   )
 }
 
--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,25 +1,12 @@
 import React from "react"
 
 type ButtonProps = {    //added this type, so that I can specify that the props for the button are of type buttonProps
-    // style: {
-    //     backgroundColor: string;
-    //     fontSize: number;
-    //     textColor: string;
-    // } instead of doing this, we can use one of the types we get from REACT.
-    style: React.CSSProperties
-    borderRadius: Record<'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight', number>
+    children: React.ReactNode;
 }
-
-export default function Button({style, borderRadius}: ButtonProps) {
+export default function Button({ children }: ButtonProps) {
     return (
-        <button style={{
-            ...style,   //spread operator 
-            borderTopLeftRadius: borderRadius.topLeft + 'px',
-            borderTopRightRadius: borderRadius.topRight + 'px',
-            borderBottomLeftRadius: borderRadius.bottomLeft + 'px',
-            borderBottomRightRadius: borderRadius.bottomRight + 'px',
-        }}>
-            Cliqueame
+        <button>
+            {children}
         </button>
     )
 }
