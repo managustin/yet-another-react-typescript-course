2342f3ed9cd1a01f262c21777ab8a27f5b85cfb6
Agustín <managustin01@gmail.com>
2025-04-22 15:41:57 -0300

    using children a second time

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import {useState} from 'react'
 import './App.css'
 import Button from './components/button'
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <main>
-
-      <Button>Cliqueame</Button>
+      <h1>Count: {count}</h1>
+      <Button setCount={setCount}> hola</Button>
 
     </main>
   )
--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,11 +1,13 @@
-import React from "react"
+import React, { Children } from "react"
 
 type ButtonProps = {    //added this type, so that I can specify that the props for the button are of type buttonProps
+    setCount: React.Dispatch<React.SetStateAction<number>>;
     children: React.ReactNode;
 }
-export default function Button({ children }: ButtonProps) {
+export default function Button({ setCount, children }: ButtonProps) {
+    
     return (
-        <button>
+        <button onClick={() => setCount(prev=> prev + 1)}>
             {children}
         </button>
     )
