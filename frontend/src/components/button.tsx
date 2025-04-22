45f9e6b07447dfa96a05099b4a756fe4abc5fe0e
Agustín <managustin01@gmail.com>
2025-04-22 10:49:33 -0300

    setting styles

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,23 +1,29 @@
+type Color = "red" | "blue" | "green" | "yellow" | "purple" | "black"
+
+type ButtonProps = {    //added this type, so that I can specify that the props for the button are of type buttonProps
+    backgroundColor: Color
+    fontSize?: number
+    pillShape?: boolean
+    textColor: Color
+    padding: [number, number, number, number]
+}
+
 export default function Button({    //added some destructuring for the props, which is a common practice
     backgroundColor,
+    textColor,
     fontSize,
     pillShape,
-}:  {
-    backgroundColor: string
-    fontSize: number
-    pillShape: boolean
-}) {
-
-    console.log('Valor de pillShape: ', pillShape)
-    if (pillShape){
-        return (
-            <button style={{background: backgroundColor, fontSize: fontSize}} className="text-white rounded-[50px] px-4 py-2 border-2">
-                Cliqueame
-            </button>
-        )
-    }
+    padding,
+}:  ButtonProps) {
     return (
-        <button style={{background: backgroundColor, fontSize: fontSize}} className="text-white rounded px-4 py-2">
+        <button style={{
+                background: backgroundColor,
+                fontSize: fontSize, 
+                color: textColor,
+                padding: `${padding[0]}px ${padding[1]}px ${padding[2]}px ${padding[3]}px`
+                }} 
+                className={`text-white border-2 ${pillShape ? 'rounded-[50px]' : 'rounded'}`}
+                >
             Cliqueame
         </button>
     )
