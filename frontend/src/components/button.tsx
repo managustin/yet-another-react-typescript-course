e4935c31ef8da045814e9a02ce7c9ce7f99d120b
Agustín <managustin01@gmail.com>
2025-05-01 17:18:24 -0300

    useState & useRef hooks

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,6 +1,23 @@
+import React, { useRef, useState } from "react";
+
+type User = {
+    name: string;
+    age: number;
+}
+
 export default function Button() {
+    
+    const [count, setCount] = useState<number>(0);
+    const [text, setText] = useState<string>("Click me!");
+    const [isPrimary, setIsPrimary] = useState(true);  //the specification of <boolean> could be added, though it's not necessary 
+    const [user, setUser] = useState<User | null>(null);    //initially, the user is null
+    
+    const name = user?.name;
+
+    const ref = useRef<HTMLButtonElement>(null);
+
     return (
-    <button onClick={(e) => console.log("clicked")}>
+    <button ref={ref}>
         Click me!
     </button>
     );
