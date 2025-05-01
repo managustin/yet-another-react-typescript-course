ccf782d4009cbb0846dab2a8921daf7f1b874e8f
Agustín <managustin01@gmail.com>
2025-05-01 18:07:54 -0300

    as type assertion

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,14 +1,19 @@
-const buttonTextOption = [
-    "Click me",
-    "click me again",
-    "click me one more time"
-] as const; // this sets these specific strings as options
+import React, { useEffect } from "react"
+
+type User = {
+    sessionId: string;
+    name: string
+}
+
+type Guest = Omit<User, "name">; //Omit takes an existing type and removes something from there 
+
+type ButtonColor = "red" | "blue" | "green";
 
 export default function Button() {
-    
-    return <button>{
-        buttonTextOption.map(option => {
-            return option;
-        })
-    }</button>
+
+    useEffect(() => {
+        const previousButtonColor = localStorage.getItem("buttonColor") as ButtonColor;
+    }, []);
+    return <button>cliqueame</button>
+
 }
