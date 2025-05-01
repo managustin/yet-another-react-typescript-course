bf32cb55a1bf898a2d411ab3a0ae5c7c983ba609
Agustín <managustin01@gmail.com>
2025-05-01 17:32:22 -0300

    as const

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,24 +1,14 @@
-import React, { useRef, useState } from "react";
-
-type User = {
-    name: string;
-    age: number;
-}
+const buttonTextOption = [
+    "Click me",
+    "click me again",
+    "click me one more time"
+] as const; // this sets these specific strings as options
 
 export default function Button() {
     
-    const [count, setCount] = useState<number>(0);
-    const [text, setText] = useState<string>("Click me!");
-    const [isPrimary, setIsPrimary] = useState(true);  //the specification of <boolean> could be added, though it's not necessary 
-    const [user, setUser] = useState<User | null>(null);    //initially, the user is null
-    
-    const name = user?.name;
-
-    const ref = useRef<HTMLButtonElement>(null);
-
-    return (
-    <button ref={ref}>
-        Click me!
-    </button>
-    );
+    return <button>{
+        buttonTextOption.map(option => {
+            return option;
+        })
+    }</button>
 }
